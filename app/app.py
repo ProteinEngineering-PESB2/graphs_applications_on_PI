a1b339f956209ce6d30839ae514a786641c72a5b
baaass2 <jokerbass087@gmail.com>
2023-03-13 16:40:11 -0300

    update

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import sys
 import os
 import errno
+import collections
 from neo4j import GraphDatabase
 from neo4j.exceptions import ServiceUnavailable
 from multiprocessing import Pool
@@ -13,8 +14,7 @@ from return_querys import group_of_querys
 class App():
 
     def __init__(self):
-        self.driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "bass"), encrypted=False) 
-
+        self.driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "bassbass"), encrypted=False) 
     def close(self):
         # Don't forget to close the driver connection when you are finished with it
         self.driver.close()
@@ -23,13 +23,15 @@ class App():
         with self.driver.session(database="neo4j") as session:
             # Read transactions allow the driver to handle retries and transient errors
             
-            result = session.read_transaction(
+            result = session.execute_read(
                 self._return_graphlet, query)
 
         graphlets = []
         for i in result:
             graphlet = i['n1']['id']+"_"+i['m']['id']+"_"+i['n2']['id']
+
             graphlets.append(graphlet)
+
         return(graphlets)
             
     @staticmethod
@@ -55,20 +57,22 @@ def export_csv(data, type_graphlet, name_export):
         if e.errno != errno.EEXIST:
             raise
 
-    df_export = pd.DataFrame(data, columns=[type_graphlet])
-    df_export.to_csv("data_output/"+name_export+"/"+type_graphlet+".csv", index=False)
+    df_export = pd.DataFrame(data, columns=[str(type_graphlet+1)])
+    df_export.to_csv("data_output/"+name_export+"/"+str(type_graphlet+1)+".csv", index=False)
 
 if __name__ == "__main__":
 
     name_export = sys.argv[1]
     app = App()
 
-    #export_csv(g1,'g1',name_export)
     querys = []
-
-    for i in range(1,14):
+    graphlets = []
+    for i in range(1,2):
         querys.append(group_of_querys.querys(i))
 
     for query in querys:
-        app.buscador(query)
+        graphlet = app.buscador(query)
+        graphlets.append(graphlet)
 
+    for i in range(0,len(graphlets)):
+        export_csv(graphlets[i],i,name_export)
